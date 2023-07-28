67f57c498d871ca4bdfad8181fe5688d35aaa2c5
sovetbekoff <b.sovetbekoff@gmail.com>
2023-07-28 12:44:04 +0600

    ddk

---
+++ in.js
@@ -0,0 +1,21 @@
+function moveElevator(direction) {
+  const elevator = document.getElementById("elevator");
+  const currentFloor = parseInt(elevator.style.bottom) || 0;
+  const targetFloor =
+    direction === "up" ? currentFloor - 100 : currentFloor + 100;
+
+  if (targetFloor >= -200 && targetFloor <= 200) {
+    elevator.style.bottom = targetFloor + "px";
+  }
+}
+
+function moveToFloor() {
+  const targetFloor = document.getElementById("floorInput").value;
+  const elevator = document.getElementById("elevator");
+  const currentFloor = parseInt(elevator.style.bottom) || 0;
+  const targetPosition = (targetFloor - 1) * -100;
+
+  if (targetFloor >= 1 && targetFloor <= 10) {
+    elevator.style.bottom = targetPosition + "px";
+  }
+}
