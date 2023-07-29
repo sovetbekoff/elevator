0144b94d771dba1b2e8f7943c42c3cc37f999186
sovetbekoff <b.sovetbekoff@gmail.com>
2023-07-29 11:09:09 +0600

    second commit

--- in.js
+++ in.js
@@ -4,7 +4,7 @@ function moveElevator(direction) {
   const targetFloor =
     direction === "up" ? currentFloor - 100 : currentFloor + 100;
 
-  if (targetFloor >= -200 && targetFloor <= 200) {
+  if (targetFloor >= -200 && targetFloor <= 400) {
     elevator.style.bottom = targetFloor + "px";
   }
 }
@@ -13,9 +13,10 @@ function moveToFloor() {
   const targetFloor = document.getElementById("floorInput").value;
   const elevator = document.getElementById("elevator");
   const currentFloor = parseInt(elevator.style.bottom) || 0;
-  const targetPosition = (targetFloor - 1) * -100;
+  const targetPosition = (targetFloor + 1) * 10;
 
   if (targetFloor >= 1 && targetFloor <= 10) {
     elevator.style.bottom = targetPosition + "px";
   }
 }
+console.log("Hello Kasi");
